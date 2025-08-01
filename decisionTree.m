159e800fc4aa6feaa8372df76f32ab387f0a1363
Elena Marcos Macias <emarco05@ucm.es>
2025-08-01 17:18:56 +0200

    Categorical predictor is weird

--- decisionTree.m
+++ decisionTree.m
@@ -11,10 +11,18 @@ T_Original = readtable(['./data/' fileName]);
 T_Original = rmmissing(T_Original);  % Remove rows that contain at least one NaN
 
 % -------- Select data to use (columns with string or numerical criteria) ---------------
-target_columns = [1,1,4,22];
+target_columns = [1,22];
 ignore_columns = [2,3];
 T_Data = selectColumns (T_Original, target_columns, ignore_columns);
+T_ResultsVariable = T_Original.Death;
 
 
 % ---------- Apply decision Tree model -----------
-Mdl = fitctree(T_Data,T_Data.Death);
+Mdl = fitctree(T_Data,T_ResultsVariable, 'CategoricalPredictors', 'Genotype', 'MinParentSize',3);
+view(Mdl,'Mode','graph');
+
+
+% --------- Trial
+%label = predict(Mdl,T_Data);
+
+%setdiff(T_ResultsVariable, label)
